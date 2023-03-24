2a2bf08c573d7b9757c0c2a2b3bcd83e3aa518a5
TaronKarapetyan <taronvkarapetyan@gmail.com>
2023-03-24 10:12:49 +0400

    fix: correction

--- Task2.1_RegExp.js
+++ Task2.1_RegExp.js
@@ -1,20 +1,20 @@
 function checkLowerCase(string) {
-    const regExp = /^[a-z]*$/g
+    const regExp = /^(\w)*$/g
     return regExp.test(string)
 }
 
-console.log(checkLowerCase('abc'))
+console.log(checkLowerCase('abcD20%%'))
 /************************************************************/
-function checkUpperCase(secondString) {
-    const secRegExp = /^[A-Z]*$/g
-    return secRegExp.test(secondString)
-}
+// function checkUpperCase(secondString) {
+//     const secRegExp = /^[A-Z]*$/g
+//     return secRegExp.test(secondString)
+// }
 
-console.log(checkUpperCase("AbS"))
-/************************************************************/
-function checkNumberCase(stringNum) {
-    const thirdRegExp = /^[0-9]*$/g
-    return thirdRegExp.test(stringNum)
-}
+// console.log(checkUpperCase("AbS"))
+// /************************************************************/
+// function checkNumberCase(stringNum) {
+//     const thirdRegExp = /^[0-9]*$/g
+//     return thirdRegExp.test(stringNum)
+// }
 
-console.log(checkNumberCase("545"))
+// console.log(checkNumberCase("545"))
