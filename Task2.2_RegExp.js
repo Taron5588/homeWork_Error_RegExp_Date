18fbd868f687630e1660bb4c29c0b388169bebb3
TaronKarapetyan <taronvkarapetyan@gmail.com>
2023-03-26 14:28:44 +0500

    fix: str.length===4||6 always returns true, don't do it

--- Task2.2_RegExp.js
+++ Task2.2_RegExp.js
@@ -1,7 +1,7 @@
 function myRegExp(string) {
     regExp = /^[0-9]*$/g
-    const condition = (string.length === 4 || 6) 
+    const condition = (string.length === 4 || string.length === 6) 
         return condition ? `the pin is ${regExp.test(string)}` : ` the pin is ${false}`
 }
 
-console.log(myRegExp("012000"))
+console.log(myRegExp("00200"))
